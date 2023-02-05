050f89a63849af99b9f9bbca1dc6e45e07e7f49b
joshuabecker91 <joshua.becker91@gmail.com>
2023-02-04 19:47:47 -0800

    functional server

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -7,6 +7,18 @@ from flask_app import app
 def index():
     return render_template('index.html')
 
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
